2a55556cdd8227bb48c9358189aa933101fd304f
abhishek-m11 <join2abhishekmaurya@gmail.com>
2025-07-06 17:44:35 +0530

    Create Assignment2.py

---
+++ Assignment2.py
@@ -0,0 +1,15 @@
+#Assignment_2 task_1
+num=int(input("Enter the number: "))
+if num%2==0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number.")
+
+
+
+#Assignment_2 task_2
+
+sum1=0
+for i in range(1,51):
+    sum1+=i
+print(f"The sum of the numbers from 1 to 50 is: {sum1}")
